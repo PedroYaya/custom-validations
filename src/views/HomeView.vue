2d6eea2c0425a2c295633223b53fabe223d603d4
Pedro Yaya <pedrogabrielyaya92@gmail.com>
2023-05-11 16:28:15 -0300

    init app

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Custom Validator</h1>
+    
+  </div>
+</template>
+
+<script setup>
+import { ref, onBeforeMount, computed, watch } from 'vue';
+
+
+</script>
+
+<style lang="scss">
+
+</style>
