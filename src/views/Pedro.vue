7d13d9db66150cabb4de6042df5d4d6e1007503e
Pedro Yaya <pedrogabrielyaya92@gmail.com>
2023-05-12 14:41:05 -0300

    min char

--- src/views/Pedro.vue
+++ src/views/Pedro.vue
@@ -14,6 +14,18 @@
       <input type="number" v-model="maxAmount" placeholder="Max amount 100"/>
       <span :class="validMaxAmount ? 'valid': 'error'"> Valid: {{ validMaxAmount }} </span>
     </div>
+
+    <div> 
+      <input type="text" v-model="minChar" placeholder="Min char 10"/>
+      <span :class="validMinChar ? 'valid': 'error'"> Valid: {{ validMinChar }} </span>
+    </div>
+
+    <div> 
+      <input type="text" v-model="maxChar" placeholder="Max char 100"/>
+      <span :class="validMaxChar ? 'valid': 'error'"> Valid: {{ validMaxChar }} </span>
+    </div>
+
+
   </div>
 </template>
 
@@ -23,17 +35,38 @@ import { ref, computed } from 'vue';
 const email = ref(null);
 const minAmount = ref(null);
 const maxAmount = ref(null);
+const minChar = ref(null);
+const maxChar = ref(null);
 
-const AMOUNT_MAP = {
-  MIN: 10,
-  MAX: 100
-}
 
-const validEmail = computed(() => email.value && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))
+const AMOUNT_MAP = { MIN: 10, MAX: 100 };
 
-const validMinAmount = computed(() => minAmount.value && minAmount.value >= AMOUNT_MAP.MIN);
+const CHAR_MAP = { MIN: 10, MAX: 100 };
 
-const validMaxAmount = computed(() => maxAmount.value && maxAmount.value <= AMOUNT_MAP.MAX);
+
+const validEmail = computed(() => required(email.value) && validateEmail(email.value))
+
+const validMinAmount = computed(() => required(minAmount.value) && validateMinAmount(minAmount.value, AMOUNT_MAP.MIN));
+
+const validMaxAmount = computed(() => required(maxAmount.value) && validateMaxAmount(maxAmount.value, AMOUNT_MAP.MAX));
+
+const validMinChar = computed(() => required(minChar.value) && validateMinChar(minChar.value, CHAR_MAP.MIN));
+
+const validMaxChar = computed(() => required(maxChar.value) && validateMaxChar(maxChar.value, CHAR_MAP.MAX));
+
+// VALIDATIONS
+
+const required = val => !!val;
+
+const validateEmail = email => typeof val === 'string' && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+
+const validateMinAmount = (val, min) => typeof val === 'number' && val >= min;
+
+const validateMaxAmount = (val, max) => typeof val === 'number' && val <= max;
+
+const validateMinChar = (val, min) => typeof val === 'string' && val.length >= min;
+
+const validateMaxChar = (val, max) => typeof val === 'string' && val.length <= max;
 
 </script>
 
